db61e0a2ed587468bc83a0e43871d049f757fbd8
dever1 <dever1@zippyops.com>
2018-09-19 12:22:37 +0530

    This Will

---
+++ src/RectangleTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RectangleTest {
+  Rectangle myRectangle = new Rectangle(5,6);
+
+  public void testGetArea() {
+    assertEquals(myRectangle.getArea(), 30);
+  }
+
+  public void testGetPerimeter() {
+    assertEquals(myRectangle.getPerimeter(), 22);
+  }
+
+  public void testLength() {
+    assertEquals(myRectangle.length, 5);
+  }
+
+ public void testWidth() {
+    assertEquals(myRectangle.width, 6);
+  }
+}
